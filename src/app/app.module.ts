cf8a6d1bc3f9f31689d56de506e1f227f29446e9
Kaio Maciel <kaioreis1153@hotmail.com>
2023-11-14 16:37:54 -0300

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './pages/listar/listar.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { EditarComponent } from './pages/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    CadastrarComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
